e25861560b24d31343689e1b20008f3286a7fd80
Mel <jones.mel@gmail.com>
2019-10-10 22:19:48 +0200

    fix frontmatter typo bug

--- moody-blog/src/templates/blog-post.js
+++ moody-blog/src/templates/blog-post.js
@@ -1,28 +1,25 @@
-// Creating the (React) template
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import { graphql } from 'graphql'
+import React from "react"
+import { Helmet } from "react-helmet"
+import { graphql } from "gatsby"
 // import '../css/blog-post.css' // make it pretty
-export default function Template({
-  data, // this prop will be injected by the GraphQL query we'll writye in a bit
-}) {
-     const { markdownRemark: post } = data // data.markdownRemark holds our post data
-     return (
-       <div className="blog-post-container">
-         <Helmet title={`Moody Blogs - ${post.formatter.title}`} />
-         <div className="blog-post">
-           <h1>{post.formatter.title}</h1>
-           <div
-             className="blog-post-content"
-             dangerouslySetInnerHTML={{ __html: post.html }}
-           />
-         </div>
-       </div>
-     )
-   }
-   // Writing the GraphQL query
-   export const pageQuery = graphql`
-    query BlogPostByPath($ath: String!) {
+export default function Template({ data }) {
+  const { markdownRemark: post } = data // data.markdownRemark holds our post data
+  return (
+    <div className="blog-post-container">
+      <Helmet title={`Moody Blogs - ${post.frontmatter.title}`} />
+      <div className="blog-post">
+        <h1>{post.frontmatter.title}</h1>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+      </div>
+    </div>
+  )
+}
+// Writing the GraphQL query
+export const pageQuery = graphql`
+    query BlogPostByPath($path: String!) {
       markdownRemark(frontmatter: { path: { eq: $path   } }) {
         html
         frontmatter {
