539c76a6da9eaaf7fccbc0e0351da133b5903498
Mel <jones.mel@gmail.com>
2019-10-22 12:40:06 +0200

    initial commit & plugins catch, helmet, filesystem

--- moody-blog/gatsby-config.js
+++ moody-blog/gatsby-config.js
@@ -1,16 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Moody Blogs`,
+    description: `A Blazing Fast Blog Built with Gatsby by Mel Jones.`,
+    author: `@moodybones`,
   },
   plugins: [
+    `gatsby-plugin-catch-links`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/pages`,
+        name: `pages`,
       },
     },
     `gatsby-transformer-sharp`,
