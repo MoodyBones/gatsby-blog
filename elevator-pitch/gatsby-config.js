d3192875891390bc5c7412b3596e011c936aa7e1
Mel <jones.mel@gmail.com>
2019-12-20 14:25:50 +0100

    metasdat & social links

--- elevator-pitch/gatsby-config.js
+++ elevator-pitch/gatsby-config.js
@@ -1,3 +1,16 @@
 module.exports = {
+  siteMetadata: {
+    title: `Gatsby.js 'Elevator Pitch' theme`,
+    url: `https://github.com/jlengstorf/gatsby-theme-simple-docs`,
+    image: 'preview.jpg',
+    language: 'en',
+    description: 'coming soon...',
+    socialLinks: {
+      github: 'https://github.com/greglobinski',
+      twitter: 'https://twitter.com/greglobinski',
+      mailto: 'mailto:greglobinski@gmail.com',
+    },
+  },
+  module.exports = {
   __experimentalThemes: ['gatsby-theme-elevator-pitch']
-}
+};
